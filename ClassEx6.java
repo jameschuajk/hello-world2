cf1ffb7cf9fe9f8a1582827e9b59e39019d3e5af
jameschuajk <jameschuabiz1@gmail.com>
2019-06-15 00:21:26 +0800

    Add files via upload

---
+++ ClassEx6.java
@@ -0,0 +1,20 @@
+class A6
+{
+	int i ;
+	A6()
+	{
+		this.i = 7;
+		}
+	
+	}
+	
+class ClassEx6
+{
+	public static void main(String args[])
+	{
+		System.out.println("*** Simple class: quizes ***");
+		A6 ob1;
+		ob1 = new A6();
+		System.out.println(ob1.i);
+		}
+	}
